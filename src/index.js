f640ef9b67ee187a93041dc658f8abb7892ef80e
DeepikaHiremath <Mathad.deepika@gmail.com>
2021-03-24 12:21:47 +0530

    index

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-social/bootstrap-social.css'
 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+{/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link> */}
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
